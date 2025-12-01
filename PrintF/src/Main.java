36c2b7db7c7c86a823d3d5590b65005e8df8cedd
mohamed elsherif <mohamed.hatem97825@gmail.com>
2025-12-01 03:13:10 +0200

    feat(PrintF): add printf examples and documentation
    - Add Main.java with various System.out.printf examples demonstrating different format specifiers, precision, and flags.
    - Add README.md explaining the printf placeholders, precision, and special characters used in the examples.

--- PrintF/src/Main.java
+++ PrintF/src/Main.java
@@ -1,2 +1,22 @@
 public class Main {
+    public static  void main(String[] args)
+    {
+        String name= "mohamed";
+        int age = 27;
+        char firstChar='m';
+        double height = 1.8;
+        boolean isEmployed = true;
+        double price = +100.2222;
+        double mmm = -102020.021454;
+        System.out.printf( " hello %s\n" , name);
+        System.out.printf("your first charachter  is %c\n" , firstChar );
+        System.out.printf("your age is %d\n" , age);
+        System.out.printf("your height is %f\n" , height);
+        System.out.printf("yes he is an employes %b\n" , isEmployed);
+        System.out.printf("this is %s and he is %d years old\n " , name , age);
+        System.out.printf("the pricee is %.1f\n" ,price);
+        System.out.printf("the second %(,.2f\n", mmm);
+
+
+    }
 }
